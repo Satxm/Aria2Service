bf19dd4aaec0d156f7880cbb859e697a7f3fb6c2
Satxm <56530136+Satxm@users.noreply.github.com>
2024-01-15 10:08:31 +0800

    Add files via upload

---
+++ Aria2Service/Aria2Service.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.ServiceProcess;
+
+namespace Aria2Service
+{
+    public partial class Aria2Service : ServiceBase
+    {
+        private Process p;
+        private string dir = AppDomain.CurrentDomain.BaseDirectory;
+        private string cmd = "aria2c.exe";
+        private string arg = "--conf-path=aria2.conf";
+
+        public Aria2Service()
+        {
+            InitializeComponent();
+        }
+        public enum ServiceState
+        {
+            SERVICE_STOPPED = 0x00000001,
+            SERVICE_START_PENDING = 0x00000002,
+            SERVICE_STOP_PENDING = 0x00000003,
+            SERVICE_RUNNING = 0x00000004,
+            SERVICE_CONTINUE_PENDING = 0x00000005,
+            SERVICE_PAUSE_PENDING = 0x00000006,
+            SERVICE_PAUSED = 0x00000007,
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct ServiceStatus
+        {
+            public int dwServiceType;
+            public ServiceState dwCurrentState;
+            public int dwControlsAccepted;
+            public int dwWin32ExitCode;
+            public int dwServiceSpecificExitCode;
+            public int dwCheckPoint;
+            public int dwWaitHint;
+        };
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        private static extern bool SetServiceStatus(System.IntPtr handle, ref ServiceStatus serviceStatus);
+
+        protected override void OnStart(string[] args)
+        {
+            ServiceStatus serviceStatus = new ServiceStatus();
+            serviceStatus.dwCurrentState = ServiceState.SERVICE_START_PENDING;
+            serviceStatus.dwWaitHint = 100000;
+            SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+
+            if (!File.Exists(Path.Combine(dir, "aria2.session"))) File.Create(Path.Combine(dir, "aria2.session"));
+
+            if (p == null)
+            {
+                p = new Process();
+                p.StartInfo.WorkingDirectory = dir;
+                p.StartInfo.UseShellExecute = true;
+                p.StartInfo.FileName = cmd;
+                p.StartInfo.Arguments = arg;
+                p.Start();
+            }
+
+            serviceStatus.dwCurrentState = ServiceState.SERVICE_RUNNING;
+            SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+        }
+
+        protected override void OnStop()
+        {
+            ServiceStatus serviceStatus = new ServiceStatus();
+            serviceStatus.dwCurrentState = ServiceState.SERVICE_STOP_PENDING;
+            serviceStatus.dwWaitHint = 100000;
+            SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+
+            if (p != null)
+            {
+                Process.GetProcessById(p.Id).Kill();
+            }
+
+            serviceStatus.dwCurrentState = ServiceState.SERVICE_STOPPED;
+            SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+        }
+    }
+}
